c72b41a112dd648c1868b48a0dc495fa430f1b1f
CKMorell <99834501+CKMorell@users.noreply.github.com>
2022-02-23 22:43:29 -0500

    Add files via upload

--- mining.py
+++ mining.py
@@ -1,27 +1,43 @@
 # Imports
 import json
-from pickaxe import Pickaxe
 from player import Player
+
+# Player init
+try:
+    file = open('player.json')
+    data = json.load(file)
+    character_name = data['Username']
+except:
+    character_name = input("Enter your character name: ")
+
+Player = Player(character_name)
 
 # Inventory init
 Inventory = Player.Inventory()
 
 # Pickaxe init
-WoodenPick = Pickaxe.WoodenPick(0, 0, 0, 0)
-StonePick = Pickaxe.StonePick(0, 0, 0, 0)
-IronPick = Pickaxe.IronPick(0, 0, 0, 0)
-GoldPick = Pickaxe.GoldenPick(0, 0, 0, 0)
-DiamondPick = Pickaxe.DiamondPick(0, 0, 0, 0)
-NetheritePick = Pickaxe.NetheritePick(0, 0, 0, 0)
+Pickaxe = Player.Pickaxe("Wooden", 1, 59, 0, 0, 0, 0)
 
+# Game loop
 running = True
 
 def main():
     Inventory.DisplayInv()
 
     while running:
-        command = input("---> ")
-        Inventory.CommandHandle(command)
+        command = input(f"\n{character_name}~> ")
+        if (command == "save"):
+            Pickaxe.SavePickaxe()
+            Inventory.CommandHandle(command)
+        elif (command == "exit"):
+            Pickaxe.SavePickaxe()
+            Inventory.CommandHandle(command)
+        elif (command == "picktype"):
+            Pickaxe.DisplayPick()
+            Inventory.CommandHandle(command)
+        else:
+            Inventory.CommandHandle(command)
+
         # Inventory.DisplayInv()
 
 main()
--- player.py
+++ player.py
@@ -1,22 +1,43 @@
-import os
-from pickaxe import Pickaxe
+import os, json
 
 class Player:
+    def __init__(self, character_name):
+        self.character_attributes = {
+            'Username': character_name
+        }
+
+        with open('player.json', 'w') as attributes:
+            json.dump(self.character_attributes, attributes)
+            attributes.close()
+
     class Inventory:
         def __init__(self):
-            self.money = 0
-            self.stone = 0
-            self.coal = 0
-            self.iron = 0
-            self.gold = 0
-            self.diamond = 0
-            self.redstone = 0
-            self.lapis = 0
+            try:
+                file = open('inventory.json')
+                inv = json.load(file)
+                self.emeralds = inv['Emeralds']
+                self.stone = inv['Stone']
+                self.coal = inv['Coal']
+                self.iron = inv['Iron']
+                self.gold = inv['Gold']
+                self.diamond = inv['Diamond']
+                self.redstone = inv['Redstone']
+                self.lapis = inv['Lapis'] 
+                print("Inventory loaded successfully!")        
+            except:
+                self.emeralds = 0
+                self.stone = 0
+                self.coal = 0
+                self.iron = 0
+                self.gold = 0
+                self.diamond = 0
+                self.redstone = 0
+                self.lapis = 0
         
         def DisplayInv(self):
-            print("INVENTORY\n----------")
+            print("\nINVENTORY\n----------")
             self.inventory = {
-                'Money': f"${self.money}\n",
+                'Emeralds': self.emeralds,
                 'Stone': self.stone,
                 'Coal': self.coal,
                 'Iron': self.iron,
@@ -29,23 +50,91 @@ class Player:
             for item, value in self.inventory.items():
                 print(f"{item}: {value}")
 
+        def Save(self):
+            with open("inventory.json", "w") as inv:
+                json.dump(self.inventory, inv)
+                inv.close()
+                print("Inventory saved!")
+
+        def Mine(self):
+            self.coal += 10
+            self.DisplayInv()
+
         def CommandHandle(self, command):
             self.commandlist = [
                 "help",
                 "exit",
-                "clear"
+                "clear",
+                "mine",
+                "inv",
+                "save",
+                "picktype"
             ]
 
-            # Handles command line input
-            if (command == self.commandlist[0]):
-                self.commandlist = "\n".join(self.commandlist)
-                print(f"\nCommands:\n----------\n{self.commandlist}")
-            elif (command == self.commandlist[1]):
-                exit(0)
-            elif (command == self.commandlist[2]):
-                if os.name in ('nt', 'dos'):
-                    os.system('cls')
-                else:
-                    os.system('clear')
-            else:
-                print(f"'{command}' is an unknown command.\n")
+            match (command):
+                case "help":
+                    self.commandlist = "\n".join(self.commandlist)
+                    print(f"\nCommands:\n----------\n{self.commandlist}")
+                case "exit":
+                    self.Save()
+                    exit(0)
+                case "clear":
+                    if os.name in ('nt', 'dos'):
+                        os.system('cls')
+                    else:
+                        os.system('clear')
+                case "mine":
+                    self.Mine()
+                case "inv":
+                    self.DisplayInv()
+                case "save":
+                    self.Save()
+                case "picktype":
+                    pass
+                case _:
+                    print(f"'{command}' is an unknown command")
+
+    class Pickaxe:
+        def __init__(self, material, cost, durability, efficiency, fortune, mending, unbreaking):
+            self.material = material
+            self.cost = cost
+            self.durability = durability
+            self.efficiency = efficiency
+            self.fortune = fortune
+            self.mending = mending
+            self.unbreaking = unbreaking
+
+            try:
+                file = open("picktype.json", "r")
+                pick = json.load(file)
+                self.type = {
+                    "Material": pick['Material'],
+                    "Durability": pick['Durability'],
+                    "Efficiency": pick['Efficiency'],
+                    "Fortune": pick['Fortune'],
+                    "Mending": pick['Mending'],
+                    "Unbreaking": pick['Unbreaking']
+                }
+                print("Pickaxe loaded successfully!")
+            except:
+                self.type = {
+                    "Material": self.material,
+                    "Durability": self.durability,
+                    "Efficiency": self.efficiency,
+                    "Fortune": self.fortune,
+                    "Mending": self.mending,
+                    "Unbreaking": self.unbreaking
+                }
+
+        def SavePickaxe(self):
+            with open("picktype.json", "w") as pick:
+                json.dump(self.type, pick)
+                pick.close()
+                print("Pickaxe saved!")
+
+        def DisplayPick(self):
+            for item, value in self.type.items():
+                print(f"{item}: {value}")
+
+        def HandleEnchantments(self, enchantment):
+            pass
